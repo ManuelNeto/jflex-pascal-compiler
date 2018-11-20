5a44cf92bdad23b49a032a0b1e747cf78837ef50
Daniyel Rocha <daniyel.rocha@ccc.ufcg.edu.br>
2018-11-20 20:50:52 -0300

    add missing files

---
+++ spec/pascal.flex
@@ -0,0 +1,137 @@
+package generated;
+import java_cup.runtime.*;
+import core.PascalSymbol;
+
+%%
+
+%public
+%class Scanner
+%cup
+
+%{
+
+   StringBuffer string = new StringBuffer();
+
+private Symbol symbol(int type) {
+	return new PascalSymbol(type, yyline+1, yycolumn+1);
+ }
+
+private Symbol symbol(int type, Object value) {
+	return new PascalSymbol(type, yyline+1, yycolumn+1, value);
+ }
+  
+%}
+
+/* Comments */
+startComment = \{ 
+endComment = \}
+contentComment= [^}]*
+Comment = {startComment}{contentComment}{endComment}
+
+/* Integer  */
+Sign = "+" | "-"
+DecimalLiteral 	= 0 | [1-9][0-9]*
+DigitSequence 	= {Sign}? {DecimalLiteral}
+IntegerNumber 	= {DigitSequence}
+
+/* Real */
+RealLiteral = [0-9][0-9]*
+RealNumber = {DigitSequence}"."{RealLiteral}
+
+/*String*/
+String = "'"[^\n\r\']+"'"
+
+/* Identifiers */
+Identifier = [:jletter:][:jletterdigit:]+
+Number = {IntegerNumber}
+
+
+/* White spaces*/
+LineTerminator = \r|\n|\r\n
+WhiteSpace     = {LineTerminator} | [ \t\f]
+
+
+%%
+
+
+<YYINITIAL> {
+
+	/* Reserved Words */
+	"program"                      	{ return symbol(sym.PROGRAM); }
+	"begin"						   	{ return symbol(sym.BEGIN); }
+	"and"							{ return symbol(sym.AND); }
+	"array"							{ return symbol(sym.ARRAY); }
+	"case"							{ return symbol(sym.CASE); }
+	"const"						   	{ return symbol(sym.CONST); }
+	"do"							{ return symbol(sym.DO); }
+	"if"							{ return symbol(sym.IF); }
+	"else"							{ return symbol(sym.ELSE); }
+	"for"							{ return symbol(sym.FOR);}
+	"function"					   	{ return symbol(sym.FUNCTION); }
+	"not"							{ return symbol(sym.NOT); }
+	"or"							{ return symbol(sym.OR); }
+	"procedure"						{ return symbol(sym.PROCEDURE); }
+	"program"						{ return symbol(sym.PROGRAM); }
+	"repeat"						{ return symbol(sym.REPEAT); }
+	"set"							{ return symbol(sym.SET); }
+	"then"							{ return symbol(sym.THEN); }
+	"type"							{ return symbol(sym.TYPE); }
+	"var"                          	{ return symbol(sym.VAR); }
+	"while"							{ return symbol(sym.WHILE); }
+	"string"						{ return symbol(sym.STRINGTERMINAL); }
+	"end"                          	{ return symbol(sym.END); }
+
+    /* Booleans */
+
+	/* Other */
+
+    /* Identifier*/
+    {Identifier} 					{ return symbol(sym.IDENTIFIER, yytext());}
+    
+    /* Separators */
+    ";"                             { return symbol(sym.SEMICOLON); }
+    "."   		  				    { return symbol(sym.DOT); }
+    ":"   		  				    { return symbol(sym.COLON); }
+    "("								{ return symbol(sym.LPAREN); }
+    ")"								{ return symbol(sym.RPAREN); }
+    ","								{ return symbol(sym.COMMA); }
+
+    /* String literal */
+    
+    {String}                        { return symbol(sym.STRING,new String(yytext())); }
+    
+    /* Number literal */
+    {IntegerNumber}                 { return symbol(sym.INTEGER_NUMBER, new Integer(yytext())); }
+    {RealNumber}                    { return symbol(sym.REAL_NUMBER, new Float(yytext())); }
+    
+	
+    /* Character literal */
+
+    /* White spaces */
+    {WhiteSpace}				    { /*Ignore*/ }
+    
+    /*Comments*/
+    {Comment}						{ /*Ignore*/ }
+    
+    /* Logical Operators*/
+    "=="							{ return symbol(sym.EQEQ); }
+    "!="							{ return symbol(sym.DIF); }
+    ">="							{ return symbol(sym.GTEQ); }
+    "<="							{ return symbol(sym.LTEQ); }
+    "<"								{ return symbol(sym.LT); }
+    ">"								{ return symbol(sym.GT); }
+    "="								{ return symbol(sym.EQ); }
+    
+    /* Arithmetical operators*/
+    "+"  							{ return symbol(sym.PLUS); }
+    "-" 							{ return symbol(sym.MINUS); }
+    "*"								{ return symbol(sym.MULT); }
+    "/"								{ return symbol(sym.DIV); }
+    
+    /* Operators */
+    
+    /* Relational Operators*/	
+
+    /* Assignment */
+    
+}
---
+++ test/lexical/PascalLexicalTest.java
@@ -0,0 +1,193 @@
+package lexical;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Paths;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import core.GeneratorLexical;
+import generated.Scanner;
+import java_cup.runtime.Symbol;
+import generated.sym;
+
+public class PascalLexicalTest {
+
+	private static String rootPath;
+	private static String subPath;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		// Generate New Lexical Analyzer with pascal.lex
+		GeneratorLexical.main(null);
+		rootPath = Paths.get("").toAbsolutePath().toString();
+		subPath = "/pascal/";
+
+		System.out.println("Setup OK");
+	}
+
+	@Test
+	public void helloTest() {	
+        //Load Hello Lexical
+		System.out.println("INICIO Hello");
+		String testCode1Path = rootPath + subPath + "/hello.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+				symb = lexer.next_token();
+			}
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void ifTest() {	
+        //Load If Lexical
+		System.out.println("INICIO If");
+		String testCode1Path = rootPath + subPath + "/if.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				symb = lexer.next_token();
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+			}
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void addTest() {	
+        //Load Add Lexical
+		System.out.println("INICIO Add");
+		String testCode1Path = rootPath + subPath + "/add.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				symb = lexer.next_token();
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	
+	@Test
+	public void setTest() {	
+        //Load Add Lexical
+		System.out.println("INICIO Set");
+		String testCode1Path = rootPath + subPath + "/set.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				symb = lexer.next_token();
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void logicalOprTest() {	
+        //Load Add Lexical
+		System.out.println("INICIO Logical Opr");
+		String testCode1Path = rootPath + subPath + "/logicalOpr.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				symb = lexer.next_token();
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void exemploGit() {	
+        //Load Add Lexical
+		System.out.println("INICIO Logical Opr");
+		String testCode1Path = rootPath + subPath + "/exemploGit.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				symb = lexer.next_token();
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testLex() {	
+        //Load Hello Lexical
+		String testCode1Path = rootPath + subPath + "/testLex.pas";
+		try {
+			String rootPath = Paths.get("").toAbsolutePath().toString();
+			FileInputStream stream = new FileInputStream(testCode1Path);
+			Reader reader = new InputStreamReader(stream);
+			Scanner lexer = new Scanner(reader);
+
+			Symbol symb = lexer.next_token();
+			while (symb.sym != sym.EOF) {
+				System.out.println(symb + " Name: " + sym.terminalNames[symb.sym]);
+				symb = lexer.next_token();
+			}
+		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+
+
+
+}
